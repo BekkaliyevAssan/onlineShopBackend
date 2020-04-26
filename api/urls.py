9f0457a2389a4222483e195bf2817e13717d3c54
Aruka Bk <arukabk@MacBook-Pro-4.local>
2020-04-26 22:02:04 +0600

    commit

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from api import views
-
+from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path('categories/', views.categories),
-
+    path('user/login/', obtain_jwt_token),
+    path('products/', views.ProductV.as_view()),
+    path('products/<int:id>/', views.ProductDV.as_view()),
     path('products/subcategory/', views.by_subcat),    
     path('products/orders/', views.OrderV.as_view()),
     path('products/<int:id>/', views.by_category),
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from django.http.response import JsonResponse
 from rest_framework import status
 from rest_framework.views import APIView
@@ -12,9 +12,14 @@ from rest_framework.decorators import permission_classes
 from api.serializers import CategoryS, SubcategoryS, ProductS
 from api.models import Category, SubCategory, Product, Order
 
+@permission_classes(IsAuthenticated)
 @api_view(['GET'])
 def categories(request):
     return JsonResponse(CategoryS(Category.objects.all(), many=True).data, safe=False)
+@permission_classes(IsAuthenticated)
+@api_view(['GET'])
+def subcategories(request):
+    return JsonResponse(SubcategoryS(SubCategory.objects.all(), many=True).data, safe=False)
 
 @api_view(['GET'])
 def by_category(request, id):
@@ -35,3 +40,35 @@ class OrderV(APIView):
             address = request.data.get('address')
         )
         return JsonResponse({"asd": "asdas"}, safe=False)
+
+class ProductV(APIView):
+    def get(self, request):
+        products=Product.objects.all()
+        return JsonResponse(ProductS(products, many=True).data, safe=False)
+
+    def post(self, request):
+        category = Category.objects.get(name=request.data['category'])
+        subcategory = SubCategory.objects.get(name=request.data['subcategory'])
+
+        Product.objects.create(
+            name = request.data['name'],
+            category = category,
+            subcategory = subcategory,
+            image = request.data['image'],
+            price = request.data['price']
+        )
+        return JsonResponse({"hello":'hello'}, safe=False)
+
+class ProductDV(APIView):
+    def put(self, request, id):
+        product = Product.objects.get(id=request.data['id'])
+        product.name = request.data['name']
+        product.image = request.data['image']
+        product.price = request.data['price']
+        product.save()
+        return JsonResponse({"hello":"hello"}, safe=False)        
+
+    def delete(self, request, id):
+        product = Product.objects.get(id=id)
+        product.delete()
+        return JsonResponse({"hello":"hello"}, safe=False)        
